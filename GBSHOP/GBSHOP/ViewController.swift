760f4d8d8b4808b220acbca34aa40cc86339011e
Sunrizz <sunrizz@icloud.com>
2020-07-15 21:22:47 +0300

    # - init progect

---
+++ GBSHOP/GBSHOP/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GBSHOP
+//
+//  Created by Алексей Усанов on 15.07.2020.
+//  Copyright © 2020 Алексей Усанов. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
